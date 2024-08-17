dd5a6ea36e24c582944fb518a750991bbeecab4b
nurfaizfoat <nurfaiz.foat100@gmail.com>
2024-08-17 16:18:01 +0800

    feat: add small js for swapping GIF avatar while hover

---
+++ themes/neobrute/assets/js/swapAvatar.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {  
+    const img = document.getElementById('hoverImage'); // Make sure this ID matches your image  
+
+    img.addEventListener('mouseover', () => {  
+        img.src = './images/nrfz_avatar.gif'; // Change to GIF  
+    });  
+
+    img.addEventListener('mouseout', () => {  
+        img.src = './images/nrfz_avatar.png'; // Change back to static image  
+    });  
+});  
